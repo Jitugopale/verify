2c1094d7d2dc64a11e2f269386d0f30e90a23bc4
Jitugopale <107456035+Jitugopale@users.noreply.github.com>
2024-11-29 22:22:29 +0530

    Update Register.js backend

--- Frontend/src/components/Register.js
+++ Frontend/src/components/Register.js
@@ -159,7 +159,7 @@ const Register = () => {
     setLoading(true); 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/createUser",
+        "https://dashboard-backend-rhfy.onrender.com/api/auth/createUser",
         formData
       );
 
