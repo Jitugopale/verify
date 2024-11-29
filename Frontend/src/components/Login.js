0af13e7f4745e81070bff0f0d0b78be9d19e10ac
Jitugopale <107456035+Jitugopale@users.noreply.github.com>
2024-11-29 22:24:16 +0530

    Update Login.js backend

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -91,7 +91,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const response = await axios.post('https://dashboard-backend-rhfy.onrender.com/api/auth/login', { email, password });
 
       if (response.data.authToken) {
         localStorage.setItem('token', response.data.authToken);
